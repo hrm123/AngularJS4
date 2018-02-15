2306d20dc30a455d722e63f8d3040f6e5b690a7e
hrm123 <bestcoder0@yahoo.com>
2018-02-15 12:20:10 -0500

    delta

--- ng5RRnd/src/app/home/home.component.ts
+++ ng5RRnd/src/app/home/home.component.ts
@@ -13,7 +13,14 @@ import {trigger, style, transition, animate, keyframes, query, stagger} from '@a
           animate('.6s ease-in', keyframes([
             style({opacity: 0, transform: 'translate(-75%)', offset: 0}),
             style({opacity: .5, transform: 'translate(35px)', offset: 0.3}),
-            style({opacity: 0, transform: 'translate(0)', offset: 1})
+            style({opacity: 1, transform: 'translate(0)', offset: 1})
+          ]))
+        ])),
+        query(':leave', stagger('300ms', [
+          animate('.6s ease-in', keyframes([
+            style({opacity: 1, transform: 'translateY(0)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)', offset: 0.3}),
+            style({opacity: 0, transform: 'translateY(-75%)', offset: 1})
           ]))
         ]))
       ])
@@ -25,7 +32,7 @@ export class HomeComponent implements OnInit {
   itemCount: number;
   btnText = 'Add an item';
   goalText = 'My first life goal';
-  goals = [];
+  goals = ['item1', 'item2', 'item3'];
   constructor() { }
 
   ngOnInit() {
@@ -36,6 +43,10 @@ export class HomeComponent implements OnInit {
     this.goals.push(this.goalText);
     this.goalText = '';
     this.itemCount = this.goals.length;
+  }
+
+  removeItem() {
+    this.goals.splice(i, 1);
   }
 
 }
